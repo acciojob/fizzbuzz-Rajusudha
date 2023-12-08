d068de837cc4ecfbb21e187d1335019d634db37f
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-08 09:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+// Initialize an empty string to store the result
+var result = "";
+
+// Loop through numbers from 1 to 20
+for (var i = 1; i <= 20; i++) {
+  // Check for multiples of both three and five
+  if (i % 3 === 0 && i % 5 === 0) {
+    result += "FizzBuzz";
+  }
+  // Check for multiples of three
+  else if (i % 3 === 0) {
+    result += "Fizz";
+  }
+  // Check for multiples of five
+  else if (i % 5 === 0) {
+    result += "Buzz";
+  }
+  // For other numbers, append the number itself as a string
+  else {
+    result += i.toString();
+  }
+
+  // Add a newline character ("\n") to separate numbers/strings
+  result += "\n";
+}
+
+// Display the result using alert
+alert(result);
